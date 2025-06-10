9c5970b4edc03bc476f99a919a69c39507614781
ecorising <norouzi@ecospatialtech.com>
2025-06-09 20:50:40 -0400

    Delete components/components/ui/cards.tsx

--- components/components/ui/cards.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export function Card({ children }: { children: ReactNode }) {
-  return <div className="rounded-lg shadow-lg border p-4">{children}</div>;
-}
-
-export function CardContent({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
-}
