aa27dcf1b318a9369f678e5ff2d6e2ccaf64b4b5
ecorising <norouzi@ecospatialtech.com>
2025-06-09 22:05:33 -0400

    Create page.tsx

---
+++ app/pages/projects/contact/page.tsx
@@ -0,0 +1,9 @@
+// pages/contact.tsx
+export default function ContactPage() {
+  return (
+    <main className="bg-green-50 text-center py-12 px-4">
+      <h2 className="text-2xl font-bold mb-4">Contact Us</h2>
+      <p>If you’d like to learn more about our services or discuss a project, contact us at <a href="mailto:info@ecospatialtech.com" className="text-green-700 font-medium">info@ecospatialtech.com</a>.</p>
+    </main>
+  );
+}
